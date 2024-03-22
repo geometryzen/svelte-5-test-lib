66d6fca59fe5a6a4f603111fcacbbf55fa96dfe3
David Geo Holmes <geometryzen@gmail.com>
2024-03-22 11:23:30 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    let name = $state("World")
+
+    $effect(() => {
+        return () => {
+        };
+    });
+    
+</script>
+
+<h1>Svelte 5 Test Library</h1>
+<h3>Hello, {name}!</h3>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+</style>
